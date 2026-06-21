c9cad6890999c5ea840aebdf1fb06c315db472cb
Y-tomo-K <web0704ty@gmail.com>
2026-06-22 03:13:38 +0900

    fix createUser

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -8,10 +8,7 @@ import { useActionState } from "react";
 import { authenticate } from "@/lib/actions/authenticate"; // ServerAction
 
 export function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined,
-  );
+  const [errorMessage, formAction] = useActionState(authenticate, undefined);
   return (
     <Card className="w-full max-w-md mx-auto">
       <CardHeader>
